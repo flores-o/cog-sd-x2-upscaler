669457b66c9506df620c07068d4f4f59d930b208
flores-o <oana.florescu28@gmail.com>
2023-02-19 21:55:46 +0000

    [1/n] Use xformers for  Memory Efficient Attention

--- predict.py
+++ predict.py
@@ -26,7 +26,7 @@ SAFETY_MODEL_ID = "CompVis/stable-diffusion-safety-checker"
 class Predictor(BasePredictor):
     def setup(self):
         """Load the model into memory to make running multiple predictions efficient"""
-        print("Loading pipeline...")
+        # print("Loading pipeline...")
         safety_checker = StableDiffusionSafetyChecker.from_pretrained(
             SAFETY_MODEL_ID,
             cache_dir=MODEL_CACHE,
@@ -37,6 +37,7 @@ class Predictor(BasePredictor):
             safety_checker=safety_checker,
             cache_dir=MODEL_CACHE,
             local_files_only=True,
+            #torch_dtype=torch.float16,
         ).to("cuda")
 
     @torch.inference_mode()
@@ -68,7 +69,7 @@ class Predictor(BasePredictor):
         """Run a single prediction on the model"""
         if seed is None:
             seed = int.from_bytes(os.urandom(2), "big")
-        print(f"Using seed: {seed}")
+        # print(f"Using seed: {seed}")
 
         # if width * height > 786432:
         #     raise ValueError(
@@ -78,6 +79,19 @@ class Predictor(BasePredictor):
         self.pipe.scheduler = make_scheduler(scheduler, self.pipe.scheduler.config)
 
         generator = torch.Generator("cuda").manual_seed(seed)
+
+
+        import psutil
+        # Measure the memory usage before inference
+        process = psutil.Process()
+        mem_before = process.memory_info().rss / 1024 / 1024  # in MB
+
+        # Memory Efficient Attention
+        self.pipe.enable_xformers_memory_efficient_attention()
+
+        import time
+        start_time = time.time()
+
         output = self.pipe(
             prompt=prompt,
             image=Image.open(image),
@@ -85,6 +99,38 @@ class Predictor(BasePredictor):
             generator=generator,
             num_inference_steps=num_inference_steps,
         )
+
+        end_time = time.time()
+        latency = end_time - start_time
+        print(f"Latency: {latency:.4f} seconds")
+        
+        with open('performance_measurement.txt', 'a') as f:
+            f.write(f"\n")
+            f.write(f"self.pipe = (... torch_dtype=torch.float16 \n")
+            f.write(f"Latency: {latency:.4f} seconds\n")
+
+        # Measure the memory usage after inference
+        mem_after = process.memory_info().rss / 1024 / 1024  # in MB
+
+        # Print the memory usage
+        mem_used = mem_after - mem_before
+
+        # import subprocess
+        # # Get the PID of the Python process
+        # pid = str(subprocess.Popen(['pgrep', '-f', 'python'], stdout=subprocess.PIPE).communicate()[0]).split()[0][2:]
+        # # Call nvidia-smi to get the GPU memory usage for the process
+        # output = subprocess.check_output(['nvidia-smi', '--query-compute-apps=pid,used_gpu_memory', '--format=csv'], encoding='utf-8')
+        # lines = output.strip().split('\n')[1:]
+        # gpu_memory_usage = [int(line.strip().split(',')[1].split()[0]) for line in lines if line.strip().split(',')[0] == pid]
+
+        
+        
+        print(f"Memory used: {mem_used:.2f} MB")
+        # print(f"GPU memory used: {gpu_memory_usage[0] / 1024**2:.2f} MB")
+
+        with open('performance_measurement.txt', 'a') as f:
+            f.write(f"Memory: {mem_used:.2f} MB\n")
+            # f.write(f"GPU memory: {gpu_memory_usage[0] / 1024**2:.2f} MB\n")
 
         output_paths = []
         for i, sample in enumerate(output.images):
